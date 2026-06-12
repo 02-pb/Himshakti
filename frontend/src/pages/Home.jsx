a2b5a0f891be777a6fb5e337261605f059153133
Priyancy Bhandari <priyancybhandari2005@gmail.com>
2026-06-12 15:20:52 +0530

    Add Himalayan products to homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,12 +6,49 @@ import Footer from "../components/Footer";
 
 export default function Home() {
   const products = [
-    { id: 1, title: "Millet Snacks", price: 40, image: "https://www.suprally.com/s/67b5c8fa10c17636cc233feb/684ad2493653fc2a2c76ae08/download-id-1m23w9fatlhpthfwtke01waqjaj3_gj13.png" },
-    { id: 2, title: "Millet Cookies", price: 60, image: "https://cdn.fcglcdn.com/brainbees/images/products/583x720/8292895a.webp" },
-    { id: 3, title: "Apple Juice", price: 80, image: "https://www.chowhound.com/img/gallery/brands-of-apple-juice-ranked-worst-to-best/intro-1728485822.jpg" },
-    { id: 4, title: "Fruit Pickle", price: 120, image: "https://honeybazzar.com/wp-content/uploads/2024/12/Mix-Fruit-image-3-scaled.jpg" },
-    { id: 5, title: "Ragi Crackers", price: 50, image: "https://cdn.grofers.com/da/cms-assets/cms/product/f15b90c2-3288-416c-9164-0df4fb9d583e.jpg" }
-  ];
+  {
+    id: 1,
+    title: "Millet Snacks",
+    price: 40,
+    image: "https://www.suprally.com/s/67b5c8fa10c17636cc233feb/684ad2493653fc2a2c76ae08/download-id-1m23w9fatlhpthfwtke01waqjaj3_gj13.png"
+  },
+  {
+    id: 2,
+    title: "Millet Cookies",
+    price: 60,
+    image: "https://cdn.fcglcdn.com/brainbees/images/products/583x720/8292895a.webp"
+  },
+  {
+    id: 3,
+    title: "Apple Juice",
+    price: 80,
+    image: "https://www.chowhound.com/img/gallery/brands-of-apple-juice-ranked-worst-to-best/intro-1728485822.jpg"
+  },
+  {
+    id: 4,
+    title: "Fruit Pickle",
+    price: 120,
+    image: "https://honeybazzar.com/wp-content/uploads/2024/12/Mix-Fruit-image-3-scaled.jpg"
+  },
+  {
+    id: 5,
+    title: "Ragi Crackers",
+    price: 50,
+    image: "https://cdn.grofers.com/da/cms-assets/cms/product/f15b90c2-3288-416c-9164-0df4fb9d583e.jpg"
+  },
+  {
+    id: 6,
+    title: "Buransh Squash",
+    price: 180,
+    image: "https://tse1.mm.bing.net/th/id/OIP.GVK6hWjtN34nxsDmPsw-HQHaEl?pid=Api&P=0&h=180"
+  },
+  {
+    id: 7,
+    title: "Millet Energy Bars",
+    price: 90,
+    image: "https://dms.mydukaan.io/original/jpeg/media/24415c3e-1dd2-495a-b9c1-f2f5f14196be.jpg"
+  }
+];
 
   const [cart, setCart] = useState({});
 
